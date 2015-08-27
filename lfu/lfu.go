202b6c2dfef5afb8e8a67e654e1f2422e060e568
shaalx <1316011195@qq.com>
2015-08-27 15:59:02 +0800

    lfu

---
+++ lfu/lfu.go
@@ -0,0 +1,173 @@
+package lfu
+
+import (
+	"fmt"
+	"sync"
+)
+
+type LFUCache struct {
+	last, latest *LFU
+	size         int
+	v            map[string]*LFU
+	sync.RWMutex
+}
+
+func NewLFUCache(size int) *LFUCache {
+	return &LFUCache{
+		size: size,
+		v:    make(map[string]*LFU),
+	}
+}
+
+func (c *LFUCache) Display() {
+	c.RLock()
+	defer c.RUnlock()
+	first := true
+	i := 0
+	for lfu := c.latest; lfu != nil; lfu = lfu.next {
+		i++
+		if i > 10 {
+			break
+		}
+		if lfu == c.latest && !first {
+			break
+		}
+		first = false
+		fmt.Print(lfu.String(), " //// ")
+	}
+	fmt.Println()
+}
+
+func (c *LFUCache) Latest() *LFU {
+	c.RLock()
+	defer c.RUnlock()
+	return c.latest
+}
+
+func (c *LFUCache) Last() *LFU {
+	c.RLock()
+	defer c.RUnlock()
+	return c.last
+}
+
+type LFU struct {
+	Key       string
+	V         interface{}
+	N         int
+	pre, next *LFU
+	sub       *subNode
+}
+
+func NewLFU(key string, v interface{}) *LFU {
+	return &LFU{
+		Key: key,
+		V:   v,
+		N:   1,
+	}
+}
+
+func (l *LFU) String() string {
+	return fmt.Sprintf("<%d times> %s:%v", l.N, l.Key, l.V)
+}
+
+type subNode struct {
+	unix int64
+}
+
+func (c *LFUCache) Get(key string) (cur *LFU) {
+	cur, exist := c.v[key]
+	if !exist {
+		return nil
+	}
+	c.Lock()
+	defer c.Unlock()
+	cur.N++
+	// only one node
+	if len(c.v) <= 1 {
+		return
+	}
+	// first node
+	if cur.pre == nil {
+		return
+	}
+	// move forward
+	if cur.N >= cur.pre.N {
+		c.moveForward(key)
+	}
+	return
+}
+
+func (c *LFUCache) Set(key string, v interface{}) (cur *LFU) {
+	c.Lock()
+	defer c.Unlock()
+	cur, exist := c.v[key]
+	if !exist {
+		cur = NewLFU(key, v)
+		// cur is the first node
+		if len(c.v) <= 0 {
+			c.latest = cur
+		} else {
+			cur.pre = c.last
+			c.last.next = cur
+		}
+		// new node is the last node
+		c.last = cur
+		c.v[key] = cur
+	} else {
+		// update cache key
+		cur.V = v
+		cur.N++
+	}
+	if cur.pre != nil && cur.pre.N <= cur.N {
+		c.moveForward(key)
+	}
+	return
+}
+
+func (c *LFUCache) moveForward(key string) {
+	cur, ok := c.v[key]
+	if !ok {
+		panic("cur key must be not nil")
+	}
+	// only one node
+	if len(c.v) <= 1 {
+		return
+	}
+	if c.latest == cur {
+		return
+	}
+	if cur.pre != nil {
+		if cur.pre.N > cur.N {
+			return
+		}
+		if c.last == cur {
+			c.last = cur.pre
+		}
+		cur.pre.next = cur.next
+		cur.pre = nil
+	}
+	// cur is the last node
+	if cur.next != nil {
+		cur.next.pre = cur.pre
+	}
+	var tmp *LFU
+	for tmp = cur.pre; tmp != nil && tmp.N <= cur.N; tmp = tmp.pre {
+	}
+	// cur will be the latest node
+	if tmp == nil {
+		c.latest.pre = cur
+		cur.next = c.latest
+		c.latest = cur
+		return
+	}
+	// insert cur after tmp
+	if tmp == cur.pre {
+		return
+	}
+	cur.pre.next = cur.next
+
+	cur.next = tmp.next
+	cur.pre = tmp
+	tmp.next.pre = cur
+	tmp.next = cur
+}
