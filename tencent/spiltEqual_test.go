001857ba6bdbb61c0da92b22b0aeaa1875801796
shaalx <wisdomswing@gmail.com>
2015-09-22 21:30:59 +0800

    spiltEqual

---
+++ tencent/spiltEqual_test.go
@@ -0,0 +1,16 @@
+package tencent
+
+import (
+	"testing"
+)
+
+func SpiltEqual(arr []int) ([]int, []int, bool) {
+	return nil, nil, false
+}
+
+func TestSE(t *testing.T) {
+	arr := []int{1, 5, 2, 6, 3, 5}
+	SpiltEqual(arr)
+	// 1+2+3-6+5-5 = 0
+	// 1,2,3,5  == 5,6
+}
