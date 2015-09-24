ebe999564a3aaac14e6ed653285bd2e8222f02ea
shaalx <wisdomswing@gmail.com>
2015-09-24 20:14:58 +0800

    test

--- tencent/spiltEqual_test.go
+++ tencent/spiltEqual_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// 把数组分为两组，使两组和相等
 func SpiltEqual(arr []int) ([]int, []int, bool) {
 	target := 0
 	for _, it := range arr {
@@ -20,17 +21,17 @@ func SpiltEqual(arr []int) ([]int, []int, bool) {
 }
 
 func dfs(i, sum, target int, arr []int, visited []bool, spiltArr []int) bool {
-	if sum+i > target {
+	if sum+arr[i] > target {
 		return false
-	} else if sum+i == target {
+	} else if sum+arr[i] == target {
 		spiltArr = append(spiltArr, arr[i])
 		fmt.Println(spiltArr)
 		return true
 	}
 	visited[i] = true
 	spiltArr = append(spiltArr, arr[i])
-	for i, it := range visited {
-		if !it && dfs(i, sum+arr[i], target, arr, visited, spiltArr) {
+	for ii, it := range visited {
+		if !it && dfs(ii, sum+arr[i], target, arr, visited, spiltArr) {
 			return true
 		}
 	}
@@ -43,6 +44,6 @@ func TestSE(t *testing.T) {
 	arr := []int{1, 5, 4, 6, 3, 5}
 	_, _, ret := SpiltEqual(arr)
 	fmt.Println(ret)
-	// 1+2+3-6+5-5 = 0
-	// 1,2,3,5  == 5,6
+	// +1 +5 +6
+	// +4 +6 +5
 }
