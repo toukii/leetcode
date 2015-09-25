a20b65fe7d435da5db38c648a6dd38216d183581
shaalx <wisdomswing@gmail.com>
2015-09-25 23:19:02 +0800

    sumEqual0

--- tencent/spiltEqual2_test.go
+++ tencent/spiltEqual2_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+var (
+	times = 0
+)
+
 // 把数组分为两组，使两组加减相等
 func SpiltEqual2(arr []int) bool {
 	Open := make(map[int]bool)
@@ -13,40 +17,39 @@ func SpiltEqual2(arr []int) bool {
 		Open[i] = true
 	}
 	spiltArr := make([]int, 0, length)
-	Open[length-1] = false
-	delete(Open)
 	ret := DFS(0, 0, arr, Open, spiltArr, true) || DFS(0, 0, arr, Open, spiltArr, false)
 	return ret
 }
 
 func DFS(i, sum int, arr []int, Open map[int]bool, spiltArr []int, flag bool) bool {
+	times++
+	fmt.Print(times, ".")
 	cur := arr[i]
 	if !flag {
 		cur *= -1
 	}
-	if condition {
+	delete(Open, i)
+	sum += cur
+	spiltArr = append(spiltArr, cur)
 
-	}
-	if sum+cur == target {
-		spiltArr = append(spiltArr, cur)
+	if len(Open) <= 0 && 0 == sum {
 		fmt.Println(spiltArr)
 		return true
 	}
-	visited[i] = true
-	spiltArr = append(spiltArr, cur)
-	for ii, it := range visited {
-		if !it && (DFS(ii, sum+cur, target, arr, visited, spiltArr, true) || DFS(ii, sum+cur, target, arr, visited, spiltArr, false)) {
+	for ii, _ := range Open {
+		if DFS(ii, sum, arr, Open, spiltArr, true) || DFS(ii, sum, arr, Open, spiltArr, false) {
 			return true
 		}
 	}
+
 	spiltArr = spiltArr[:len(spiltArr)-1]
-	visited[i] = false
+	Open[i] = true
 	return false
 }
 
 func TestSE2(t *testing.T) {
 	arr := []int{1, 5, -4, 6, 3, 5}
-	_, _, ret := SpiltEqual2(arr)
+	ret := SpiltEqual2(arr)
 	fmt.Println(ret)
 	// +1 +5 +6
 	// +4 +6 +5
